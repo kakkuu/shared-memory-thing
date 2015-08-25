024588964d528f885ef84b88ce1a6a5a92c1e27e
kakkuu <snoewflaek@gmail.com>
2015-08-26 01:00:59 +0200

    basic mmap test
    works on windwos, even 64bit, whee.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <sigsegv.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <cstring>
 using namespace std;
+
+#define ADDR ((void*)0x0000010000000000)
+
+int sigsegvhandler(void *addr, int serious)
+{
+	cout << "sigsegvhandler got called with " << addr << "!" << endl;
+	return 1; // success
+}
 
 int main()
 {
-	cerr << "meow" << endl;
+	cout << "sizeof(void*) = " << sizeof(void*) << endl;
+	
+	if (sigsegv_install_handler(sigsegvhandler)) {
+		cout << "could not install sigsegv handler" << endl;
+		_exit(-1);
+	}
+	
+	void *addr = mmap(ADDR, 10, PROT_READ | PROT_WRITE,
+		MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+	if (addr == MAP_FAILED) {
+		cout << "could not mmap. errno = " << errno
+			<< " (" << strerror(errno) << ")." << endl;
+		_exit(-1);
+	}
+	cout << "mmap returned: " << addr << endl;
 	
 	return 0;
 }
